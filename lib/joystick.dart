65260256f3043b42f7947f9fc8ddc8e7314b58ad
Dani <daniel.benet@outlook.es>
2020-12-02 18:43:02 +0100

    joystick centered and moves vertically

--- lib/joystick.dart
+++ lib/joystick.dart
@@ -7,22 +7,35 @@ class JoyStick extends StatefulWidget {
 
 class _JoyStickState extends State<JoyStick> {
   double xOffset = 0;
-  double yOffset = 0;
+  double yOffset = 75;
 
   @override
   Widget build(BuildContext context) {
-    xOffset = 100;
-    yOffset = 100;
     return Container(
-      alignment: Alignment.center,
       decoration: BoxDecoration(color: Colors.amberAccent),
       height: 200,
       width: 200,
       child: GestureDetector(
+        onVerticalDragUpdate: (details) {
+          setState(() {
+            double dy = details.localPosition.dy;
+            /* if it's within the limits of the container, move vertically */
+            if (dy >= 0 && dy <= 145) {
+              yOffset = dy;
+            }
+          });
+        },
+        onVerticalDragEnd: (details) {
+          setState(() {
+            yOffset = 75;
+          });
+        },
         child: Stack(
           children: [
             /* Posiciona relatiu al contenidor superior */
             Positioned(
+              top: yOffset,
+              left: 70,
               child: FloatingActionButton(onPressed: () {}),
             ),
           ],
