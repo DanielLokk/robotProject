b092587a20800c63d9abea4d820bd8f11adbdb8f
Dani <daniel.benet@outlook.es>
2020-12-03 09:29:15 +0100

    send 1 0 or -1 by ranges finished

--- lib/joystick.dart
+++ lib/joystick.dart
@@ -20,51 +20,67 @@ class _JoyStickState extends State<JoyStick> {
   of the joystick vertically */
   double yOffset = 75;
 
-  void updateData() {
-    //widget.database.child('left').update({'move': 1});
-    widget.database.once().then((DataSnapshot snapshot) {
+  /* sends motor update */
+  void updateData(value) {
+    widget.database.child('left').update({'move': value});
+    /* widget.database.once().then((DataSnapshot snapshot) {
       print('Data : ${snapshot.value}');
-    });
+    }); */
   }
 
   @override
   Widget build(BuildContext context) {
-    updateData();
+    double startOffset = 75;
     /* container that sets the limit of the gesture box */
-    return Container(
-      decoration: BoxDecoration(color: Colors.grey),
-      height: 200,
-      width: 200,
-      child: GestureDetector(
-        onVerticalDragUpdate: (details) {
-          setState(() {
-            double dy = details.localPosition.dy;
-            /* if it's within the limits of the container, move vertically */
-            if (dy >= 0 && dy <= 145) {
-              yOffset = dy;
-            }
-          });
-        },
-        /* when the user drag ends, the position is set to origin */
-        onVerticalDragEnd: (details) {
-          setState(() {
-            yOffset = 75;
-          });
-        },
-        child: Stack(
-          children: [
-            /* Posiciona relatiu al contenidor superior */
-            Positioned(
-              top: yOffset,
-              left: 70,
-              child: FloatingActionButton(
-                onPressed: () {},
-                elevation: 5,
-              ),
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.grey),
+          height: 200,
+          width: 200,
+          child: GestureDetector(
+            onVerticalDragUpdate: (details) {
+              setState(() {
+                double dy = details.localPosition.dy;
+                /* if it's within the limits of the container, move vertically */
+                if (dy >= 0 && dy <= 145) {
+                  yOffset = dy;
+
+                  /* updates the data depending on the offset */
+                  if (dy > startOffset + 5) {
+                    updateData(-1);
+                  } else if (startOffset - 5 <= dy && dy <= startOffset + 5) {
+                    updateData(0);
+                  } else if (dy < startOffset - 2) {
+                    updateData(1);
+                  }
+                }
+              });
+            },
+            /* when the user drag ends, the position is set to origin */
+            onVerticalDragEnd: (details) {
+              setState(() {
+                yOffset = 75;
+                updateData(0);
+              });
+            },
+            child: Stack(
+              children: [
+                /* Posiciona relatiu al contenidor superior */
+                Positioned(
+                  top: yOffset,
+                  left: 70,
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    elevation: 5,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        Text("$yOffset"),
+      ],
     );
   }
 }
