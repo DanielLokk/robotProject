dbece25576ae5d39339eb09e57cb79f74507fcb3
Dani <daniel.benet@outlook.es>
2020-12-03 10:47:54 +0100

    load live feed, without stream available

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_vlc_player/vlc_player.dart';
 import 'joystick.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+import 'package:flutter_vlc_player/vlc_player_controller.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,25 +25,76 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        /* container to align on the bottom  */
-        body: Container(
-          alignment: Alignment.bottomCenter,
-          child: Row(
-            children: [
-              /* maybe this needs to be changed later for responsiveness */
-              Padding(padding: EdgeInsets.only(left: 160)),
-              JoyStick(
-                database: database,
-                direction: JoyStick.vertical,
-              ),
-              Padding(padding: EdgeInsets.symmetric(horizontal: 100)),
-              JoyStick(
-                database: database,
-                direction: JoyStick.horizontal,
-              ),
-            ],
-          ),
+      home: HomePage(database: database),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key key,
+    @required this.database,
+  }) : super(key: key);
+
+  final DatabaseReference database;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _streamURL;
+  VlcPlayerController _vlcPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    _vlcPlayerController = new VlcPlayerController();
+    _streamURL = 'wbfejnk';
+  }
+
+  void setUrl() {
+    setState(() {
+      if (_streamURL != null) {
+        _streamURL = null;
+      } else {
+        _streamURL = '';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /* container to align on the bottom  */
+      body: Container(
+        alignment: Alignment.bottomCenter,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _streamURL == null
+                ? Container()
+                : VlcPlayer(
+                    defaultHeight: 200,
+                    defaultWidth: 200,
+                    url: _streamURL,
+                    controller: _vlcPlayerController,
+                    placeholder: Container(),
+                  ),
+            Row(
+              children: [
+                /* maybe this needs to be changed later for responsiveness */
+                JoyStick(
+                  database: widget.database,
+                  direction: JoyStick.vertical,
+                ),
+                JoyStick(
+                  database: widget.database,
+                  direction: JoyStick.horizontal,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
